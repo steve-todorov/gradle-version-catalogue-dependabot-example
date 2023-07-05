2602c9980170ff0abef67be90ced6b9a57a871a5
Steve Todorov <steve.todorov@carlspring.com>
2023-07-05 22:46:24 +0300

    More examples

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,9 +7,17 @@ version = "1.0-SNAPSHOT"
 
 dependencies {
     testImplementation(platform("org.junit:junit-bom:${settingsLibs.versions.junit.get()}")) {
-        because("This is an example which uses the settings.gradle.kts versions defined directly into the settings.gradle.kts")
+        because("This does not work yet in dependabot.")
+    }
+    testImplementation(settingsLibs.org.assertj.assertj.db) {
+        because("Testing if settingsLib works with dependabot.")
+    }
+    testImplementation(settingsLibs.org.assertj.assertj.vavr) {
+        because("Testing if settingsLib works with dependabot.")
     }
     testImplementation(fileLibs.assertj.core)
+
+    // this does not work:
     //implementation("org.apache.maven.plugins:maven-gpg-plugin:${fileLibs.versions.gpgPlugin.get()}")
 }
 
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -12,7 +12,11 @@ dependencyResolutionManagement {
 
     versionCatalogs {
         create("settingsLibs") {
-            version("junit", "5.7.0")
+            val junitVersion = "5.6.0"
+            val assertjDb = "1.1.0"
+            version("junit", junitVersion)
+            library("org.assertj.assertj-db", "org.assertj:assertj-db:${assertjDb}")
+            library("org.assertj.assertj-vavr", "org.assertj", "assertj-db").version("0.4.0") // this does not work in github yet.
         }
         create("fileLibs") {
             from(files("./gradle/libs.versions.toml"))
